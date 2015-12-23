308c44a76f71a88a6879d463b92a4c58223d9a77
Jonas Dann <jonas.chr.dann@gmail.com>
2015-12-23 12:13:36 +0100

    Added time measurement to day3-1

--- day3-1.c
+++ day3-1.c
@@ -4,12 +4,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "util/general.h"
-#include "util/present.h"
 #include "util/positionVisistedList.h"
 
 int main(int argc, char *argv[]) {
     char *input = getInput(argv[0]);
+
+    clock_t t1, t2;
+
+    t1 = clock();
 
     int totalHouses = 1;
     int x = 0;
@@ -41,7 +45,11 @@ int main(int argc, char *argv[]) {
         i++;
     }
 
-    printf("Houses: %i", totalHouses);
+    t2 = clock();
+    int time = t2 - t1;
+    printf("The answer was found in %i milliseconds\n", time);
+
+    printf("Santa visited %i houses", totalHouses);
 
     freeList(&list);
     free(input);
