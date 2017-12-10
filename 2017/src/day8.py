c1907145d9415dfb63da7b7b5c379c89ab8c61ab
Jonas Dann <jonas.dann@sva.de>
2017-12-10 16:29:11 +0100

    Solved day 8

--- 2017/src/day8.py
+++ 2017/src/day8.py
@@ -1,5 +1,7 @@
 import re
 
+import sys
+
 
 def instructionTuple(string):
     groups = re.search('([a-z]+) (inc|dec) (-?\d+) if ([a-z]+) ([!<>=]+) (-?\d+)', string).groups()
@@ -8,15 +10,20 @@ def instructionTuple(string):
 
 with open('../input/day8.txt', 'r') as file:
     inputPuzzle = file.read()
-    ops = {"<": (lambda x, y: x < y), ">": (lambda x, y: x > y), "==": (lambda x, y: x == y),
-           "<=": (lambda x, y: x <= y), ">=": (lambda x, y: x >= y), "!=": (lambda x, y: x != y)}
+    conditions = {"<": (lambda x, y: x < y), ">": (lambda x, y: x > y), "==": (lambda x, y: x == y),
+                  "<=": (lambda x, y: x <= y), ">=": (lambda x, y: x >= y), "!=": (lambda x, y: x != y)}
     instructions = list(map(instructionTuple, inputPuzzle.split("\n")))
     registers = dict()
+    highestEver = -sys.maxsize - 1
     for instruction in instructions:
-        if ops[instruction[3]](registers.get(instruction[2], 0), instruction[4]):
-            if not (instruction[0] in registers):
-                registers[instruction[0]] = 0
-            registers[instruction[0]] += instruction[1]
+        register, value, condRegister, condition, condValue = instruction
+        if conditions[condition](registers.get(condRegister, 0), condValue):
+            if not (register in registers):
+                registers[register] = 0
+            registers[register] += value
+            if registers[register] > highestEver:
+                highestEver = registers[register]
     sortedValues = list(registers.values())
     sortedValues.sort()
     print("star 1: %s" % sortedValues[-1:][0])
+    print("star 2: %s" % highestEver)
